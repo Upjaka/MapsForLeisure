d25793a7a60a4646ae23fb499efe34dafd812b25
Лудов Александр <alesant-l-99@mail.ru>
2023-11-09 19:54:35 +0300

    Добавлен маркер местоположения при запуске приложения. Добавлено добавление маркера при долгом нажатии.

---
+++ app/src/main/java/com/example/osmexample/CompassActivity.java
@@ -0,0 +1,55 @@
+package com.example.osmexample;
+
+import android.content.Context;
+import android.hardware.SensorEventListener;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class CompassActivity extends AppCompatActivity implements SensorEventListener {
+    private ImageView compassImageView;
+    private SensorManager sensorManager;
+    private Sensor magneticSensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        compassImageView = findViewById(R.id.compassImageView);
+        // Другие инициализации и настройки
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        magneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == magneticSensor) {
+            float azimuth = event.values[0]; // Угол азимута в радианах
+            // Обновите ваш пользовательский интерфейс с новым углом азимута
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Необходимо реализовать этот метод
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, magneticSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
--- app/src/main/java/com/example/osmexample/MainActivity.java
+++ app/src/main/java/com/example/osmexample/MainActivity.java
@@ -2,16 +2,39 @@ package com.example.osmexample;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.Manifest;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.location.LocationRequest;
+
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
+import org.osmdroid.events.MapEventsReceiver;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+
+import androidx.core.app.ActivityCompat;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-public class MainActivity extends Activity {
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Polyline;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity implements MapEventsReceiver {
     MapView map = null;
+    GeoPoint userLocation = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,8 +43,8 @@ public class MainActivity extends Activity {
         //handle permissions first, before map is created. not depicted here
 
         //load/initialize the osmdroid configuration, this can be done
-        Context ctx = getApplicationContext();
-        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        Context context = getApplicationContext();
+        Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context));
         //setting this before the layout is inflated is a good idea
         //it 'should' ensure that the map has a writable location for the map cache, even without permissions
         //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
@@ -38,9 +61,48 @@ public class MainActivity extends Activity {
         );
 
         map.setMinZoomLevel(3.0);
-        map.getController().setZoom(4);
+        map.getController().setZoom(15);
         map.setHorizontalMapRepetitionEnabled(true);
         map.setVerticalMapRepetitionEnabled(false);
+
+        MapEventsOverlay mapEventsOverlay = new MapEventsOverlay(this, this);
+        map.getOverlays().add(0, mapEventsOverlay);
+
+        FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // Создайте запрос местоположения
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Проверьте разрешения
+        if (checkLocationPermission()) {
+            // Разрешения на местоположение уже предоставлены
+
+            // Запросите текущее местоположение
+            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        double latitude = location.getLatitude();
+                        double longitude = location.getLongitude();
+
+                        // Ваш код для обработки полученного местоположения
+                        userLocation = new GeoPoint(latitude, longitude);
+
+                        // Добавление маркера текущего местоположения
+                        addMarker(userLocation);
+                    } else {
+                        // Местоположение не доступно, обработайте это соответственно
+                        userLocation = new GeoPoint(58.200897, 68.253976);
+                    }
+                    // Центрируйте карту на местоположении пользователя
+                    map.getController().setCenter(userLocation);
+                }
+            });
+        } else {
+            // Запрос разрешений
+            requestLocationPermission();
+        }
     }
 
     public void onResume() {
@@ -60,4 +122,63 @@ public class MainActivity extends Activity {
         //Configuration.getInstance().save(this, prefs);
         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
+
+    // Проверка наличия разрешений на доступ к местоположению
+    private boolean checkLocationPermission() {
+        int fineLocationPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int coarseLocationPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        return fineLocationPermission == PackageManager.PERMISSION_GRANTED && coarseLocationPermission == PackageManager.PERMISSION_GRANTED;
+    }
+
+    // Запрос разрешений на доступ к местоположению
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 12345);
+    }
+
+    private void addMarker(GeoPoint markerLocation) {
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(markerLocation);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+//        startMarker.setIcon(ResourcesCompat.getDrawable(getResources(),
+//                R.drawable.marker, getApplicationContext().getTheme()));
+        map.getOverlays().add(startMarker);
+        map.invalidate();
+    }
+
+    @Override
+    public boolean singleTapConfirmedHelper(GeoPoint p) {
+        return false;
+    }
+
+    @Override
+    public boolean longPressHelper(GeoPoint p) {
+        addMarker(p);
+//        RoadManager roadManager = new OSRMRoadManager(this, "MY_USER_AGENT");
+//        ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
+//        waypoints.add(userLocation);
+//        waypoints.add(p);
+//        Road road = roadManager.getRoad(waypoints);
+//        if (road.mStatus != Road.STATUS_OK)
+//            Toast.makeText(this, "Error when loading the road - status=" + road.mStatus, Toast.LENGTH_SHORT).show();
+//
+//        Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
+//        map.getOverlays().add(roadOverlay);
+        return false;
+    }
+
+    //    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        switch (requestCode) {
+//            case 12345:
+//                if (grantResults.length > 0
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    // permission granted
+//                    centerMapOnLocation();
+//                } else {
+//                    // permission denied
+//                    map.getController().setCenter(new GeoPoint(58.200897, 68.253976));
+//                }
+//                return;
+//        }
+//    }
 }
