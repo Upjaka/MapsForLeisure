b4e13f850f836b7d496bf2cd1efe752826dc1fa8
LudovDI <70688004+LudovDI@users.noreply.github.com>
2023-12-14 14:05:41 +0300

    Добавлен экран со списком сохраненных меток/маршрутов/треков. Добавлен экран с созданием/изменением меток. Также добавлены классы для отображения на экран списка меток/маршрутов/треков (ListItem, ListItemsAdapter).
    Решена задача PI-29 (https://tracker.yandex.ru/PI-29)
    Частично решена задача PI-31 (https://tracker.yandex.ru/PI-31)

---
+++ app/src/main/java/com/example/osmexample/ListItemsAdapter.java
@@ -0,0 +1,62 @@
+package com.example.osmexample;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+public class ListItemsAdapter extends ArrayAdapter<ListItem> {
+
+    public ListItemsAdapter(Context context, List<ListItem> itemList) {
+        super(context, 0, itemList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        // получение или создание ViewHolder
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_list, parent, false);
+            holder = new ViewHolder();
+            holder.imageView = convertView.findViewById(R.id.imageView);
+            holder.textView1 = convertView.findViewById(R.id.textView1);
+            holder.textView2 = convertView.findViewById(R.id.textView2);
+            holder.dateTime = convertView.findViewById(R.id.textView3);
+            holder.imageButton1 = convertView.findViewById(R.id.imageButton1);
+            holder.imageButton2 = convertView.findViewById(R.id.imageButton2);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        // установка значений элементов разметки из данных элемента списка
+        ListItem item = getItem(position);
+        if (item != null) {
+            holder.imageView.setImageResource(item.getImageId1());
+            holder.textView1.setText(item.getText1());
+            holder.textView2.setText(item.getText2());
+            holder.dateTime.setText(item.getDateTime());
+            holder.imageButton1.setImageResource(item.getImageId2());
+            holder.imageButton2.setImageResource(item.getImageId3());
+        }
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        ImageView imageView;
+        TextView textView1;
+        TextView textView2;
+        TextView dateTime;
+        ImageView imageButton1;
+        ImageView imageButton2;
+    }
+}
